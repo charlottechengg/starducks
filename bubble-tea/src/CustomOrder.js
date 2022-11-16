f40ecd6889e285063ed6b41412bbb1a96c51aa34
Senni Tan <tenseny@gmail.com>
2022-11-16 00:02:43 -0500

    finish navbar and update custom order page

--- bubble-tea/src/CustomOrder.js
+++ bubble-tea/src/CustomOrder.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ResponsiveAppBar from './NavBar';
+import Box from '@mui/material/Box';
 
 export default function CustomerOrder() {
     return (
-        <ResponsiveAppBar />
+        <>
+            <Box>
+                <ResponsiveAppBar />
+            </Box>
+            <Box sx={{ display: 'inline-flex', justifyContent: 'center' }}>
+                <Box>
+                    left Box
+                </Box>
+                <Box>
+                    right Box
+                </Box>
+            </Box>
+        </>
     );
 }
