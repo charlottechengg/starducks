7413359c199d3e86f68a3143a8a7a44c4201f18d
Junhong Chen <chenj297@mcmaster.ca>
2022-11-19 10:08:41 -0500

    add bubble tea preview image

--- bubble-tea/src/components/Menu/bubbleTeas.js
+++ bubble-tea/src/components/Menu/bubbleTeas.js
@@ -27,35 +27,30 @@ export const bubbleTeas = [
     id: 5,
     name: "bubble tea 5",
     price: "$5",
-    image:
-      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+    image: "bubbletea5.jpg",
   },
   {
     id: 6,
     name: "bubble tea 6",
     price: "$5",
-    image:
-      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+    image: "bubbletea6.png",
   },
   {
     id: 7,
     name: "bubble tea 7",
     price: "$5",
-    image:
-      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+    image: "bubbletea7.png",
   },
   {
     id: 8,
     name: "bubble tea 8",
     price: "$5",
-    image:
-      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+    image: "bubbletea8.png",
   },
   {
     id: 9,
     name: "bubble tea 9",
     price: "$5",
-    image:
-      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+    image: "bubbletea9.png",
   },
 ];
