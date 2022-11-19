b25035dcd33a813d9ce326cf368452759157d65a
Junhong Chen <chenj297@mcmaster.ca>
2022-11-19 09:13:38 -0500

    menu page

---
+++ bubble-tea/src/components/Menu/Menu.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Menu.css";
+import { bubbleTeas } from "./bubbleTeas";
+import ResponsiveNavBar from "../CustomOrder/NavBar";
+
+export default function Menu() {
+  return (
+    <div>
+      <ResponsiveNavBar />
+      <Buttons />
+      <BubbleTeaList />
+    </div>
+  );
+}
+function BubbleTeaList() {
+  return (
+    <section
+      /*setting the bubble tea list
+with->90% of the viewpoint
+margin ->5 unit relating the size of the root text
+display ->shows the items in a grid
+gap -> gap between each grip
+*/
+      className="bubbleTeaList"
+      style={{
+        width: "90vw",
+        maxWidth: "1280px",
+        margin: "5rem auto",
+        marginTop: "200px",
+        display: "grid",
+        gap: "5vw",
+      }}>
+      {bubbleTeas.map((info) => {
+        return <BubbleTea key={info.id} info={info} />;
+      })}
+    </section>
+  );
+}
+//function invoked when items are clicked
+const clickHandler = (name) => {
+  alert(name);
+};
+const onMouseOver = () => {};
+const BubbleTea = ({ info }) => {
+  const { name, price, image } = info;
+  return (
+    <article
+      className="bubbleTea"
+      onClick={() => clickHandler(name)}
+      onMouseOver={() => {}}>
+      <img src={image} alt="" width="300"></img>
+      <h1>{name}</h1>
+      <h4>{price}</h4>
+    </article>
+  );
+};
+
+function Buttons() {
+  return (
+    <div style={{ marginTop: "px" }}>
+      <ul>
+        <li>
+          <a href="#">All</a>
+        </li>
+        <li>
+          <a href="#">Black Teas</a>
+        </li>
+        <li>
+          <a href="#">Green Teas</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ bubble-tea/src/components/Menu/bubbleTeas.js
@@ -0,0 +1,65 @@
+export const bubbleTeas = [
+  {
+    id: 1,
+    name: "bubble tea 1",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+  {
+    id: 2,
+    name: "bubble tea 2",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+  {
+    id: 3,
+    name: "bubble tea 3",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+  {
+    id: 4,
+    name: "bubble tea 4",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+  {
+    id: 5,
+    name: "bubble tea 5",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+  {
+    id: 6,
+    name: "bubble tea 6",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+  {
+    id: 7,
+    name: "bubble tea 7",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+  {
+    id: 8,
+    name: "bubble tea 8",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+  {
+    id: 9,
+    name: "bubble tea 9",
+    price: "$5",
+    image:
+      "https://img.freepik.com/free-photo/minimalist-still-life-asian-tea_23-2149709056.jpg?w=1480&t=st=1668789635~exp=1668790235~hmac=85052c247c9fb2eacd8161408e48f4c81ab3896ae2ccd2709a7e0ef7fe84d05b",
+  },
+];
