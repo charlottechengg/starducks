008e38c9512fc44838442856bd005cdabbee0276
HelenC11 <59624330+HelenC11@users.noreply.github.com>
2022-11-20 05:36:18 -0500

    Update CheckOut.js

--- bubble-tea/src/components/CheckOut.js
+++ bubble-tea/src/components/CheckOut.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Checkout from "./Checkout/Checkout";
 
-export default function CheckOut() {
-    return (
-        <h>Checkout Page</h>
-    );
+const styles = {
+  fontFamily: "Helvetica",
+  textAlign: "left",
+  fontWeight: "100",
+  maxWidth: "500px",
+  margin: "0 auto"
+};
+
+const App = () => (
+  <div style={styles}>
+    <h2>Order Details</h2>
+    <Checkout />
+  </div>
+);
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+
+export default function Menu() {
+  return (
+    <App />
+
+  );
+ 
 }
